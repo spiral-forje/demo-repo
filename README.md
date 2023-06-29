891e2df4d554f8493c380cdd3f5dae927428cf2b
arnob <hello@gmail.com>
2023-06-30 04:30:04 +0530

    small change from master

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hello this is a testing project
+
 yep you heard it right
+this is a new branch in order to test the branches here
--- c_programming/my_first_c_programming.c
+++ c_programming/my_first_c_programming.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<stdio.h> 
 void main(){
-	printf("this is my first c programm actually written for git hub!!!");
+	printf("this is my first c programm actually written for git hub!!!"); 
 }
